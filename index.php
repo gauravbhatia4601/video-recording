b3f21d464bf6567700d9c13fc1cee5932ea37d0c
gauravbhatia4601 <gauravbhatia4601@users.noreply.github.com>
2022-02-12 15:25:03 +0530

    Updated video recording js

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>Video Reording</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+    </head>
+    <body> 
+        <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#video-record-modal">
+    Let's Start Recording
+  </button>
+  <style>
+    .progress-bar{
+      margin : 0 16px 0 !important;
+    }
+  </style>
+  <!-- Modal -->
+  <div class="modal fade" id="video-record-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Record Video</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <video class="video-display" autoplay playsinline muted volume="1" width="100%"></video>          
+        </div>
+        <p class="upload-status text-right"></p>
+        <div class="progress d-none">
+            <div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+        <div class="modal-footer">
+        
+          <button type="button" class="btn btn-primary start-recording-btn">Start Recording</button>
+          <button type="button" class="btn btn-secondary stop-recording-btn" disabled>Stop Recording</button>
+          <button type="button" class="btn btn-primary save-recording-btn" disabled>Save Video</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://www.webrtc-experiment.com/RecordRTC.js"></script>
+    <script src="https://www.webrtc-experiment.com/gif-recorder.js"></script>
+    <script src="https://www.webrtc-experiment.com/getScreenId.js"></script>
+    <script src="https://www.webrtc-experiment.com/DetectRTC.js"> </script>
+   <script src="./video-recording.js"></script>
+  </body>
+</html>
---
+++ video-recording.js
@@ -0,0 +1,257 @@
+const startRecording = document.querySelector('.start-recording-btn');
+const stopRecording = document.querySelector('.stop-recording-btn');
+const saveRecording = document.querySelector('.save-recording-btn');
+var recordingPlayer = document.querySelector('.video-display');
+
+//get userMedia on opening of modal
+jQuery('#video-record-modal').on('show.bs.modal',function(){
+    captureUserMedia({video: true, audio: true}, function(audioVideoStream) {
+        recordingPlayer.srcObject = audioVideoStream;        
+    }, function(error) {
+        alert(error);
+        return false;
+    });
+});
+
+
+(function() {
+    var params = {},
+        r = /([^&=]+)=?([^&]*)/g;
+
+    function d(s) {
+        return decodeURIComponent(s.replace(/\+/g, ' '));
+    }
+
+    var match, search = window.location.search;
+    while (match = r.exec(search.substring(1))) {
+        params[d(match[1])] = d(match[2]);
+
+        if(d(match[2]) === 'true' || d(match[2]) === 'false') {
+            params[d(match[1])] = d(match[2]) === 'true' ? true : false;
+        }
+    }
+
+    window.params = params;
+})();
+
+
+
+var count = 0;
+var button;
+//event listener for start recording
+startRecording.addEventListener('click',async function(){
+    button = this;
+    recordingPlayer.muted = 1;
+    recordingPlayer.controls = false;
+
+    var commonConfig = {
+        onMediaCaptured: function(stream) {
+            button.stream = stream;
+            if(button.mediaCapturedCallback) {
+                button.mediaCapturedCallback();
+            }
+            button.disabled = true;
+            stopRecording.disabled = false;
+            saveRecording.disabled = true;
+        },
+        onMediaStopped: function() {   
+            if(!button.disableStateWaiting) {
+                button.disabled = false;
+            }
+        },
+        onMediaCapturingFailed: function(error) {
+            if(error.name === 'PermissionDeniedError' && !!navigator.mozGetUserMedia) {
+                alert(error);
+            }
+    
+            commonConfig.onMediaStopped();
+        }
+    };
+
+
+
+    await captureAudioPlusVideo(commonConfig);
+
+
+    button.mediaCapturedCallback = function() {
+        button.recordRTC = RecordRTC(button.stream, {
+            type: 'video',
+            disableLogs: params.disableLogs || false,
+            canvas: {
+                width: params.canvas_width || 320,
+                height: params.canvas_height || 240
+            },
+            frameInterval: typeof params.frameInterval !== 'undefined' ? parseInt(params.frameInterval) : 20 // minimum time between pushing frames to Whammy (in milliseconds)
+        });
+        
+        
+        button.recordingEndedCallback = function(url) {
+            recordingPlayer.controls = true;
+            recordingPlayer.src = null;
+            recordingPlayer.srcObject = null;
+            recordingPlayer.remove();
+            let videoElement = document.createElement('video');
+            videoElement.setAttribute("class","video-display");
+            videoElement.autoplay = true;
+            videoElement.controls = true;
+            videoElement.volume = 1;
+            videoElement.playsInline = true;
+            videoElement.muted = 1;
+            videoElement.style.width = '100%';
+            document.querySelector('.modal-body').append(videoElement);
+            recordingPlayer = videoElement;
+            recordingPlayer.src = url;
+            recordingPlayer.onended = function() {
+                recordingPlayer.pause();
+                recordingPlayer.src = URL.createObjectURL(button.recordRTC.blob);
+            };
+        };
+        //start recording function
+        button.recordRTC.startRecording();
+    };
+
+});
+
+//stop recording listener
+stopRecording.addEventListener('click',function(){
+    this.disabled = true;
+    startRecording.disabled = false;
+    saveRecording.disabled = false;
+    function stopStream() {
+        if(button.stream && button.stream.stop) {
+            button.stream.stop();
+            button.stream = null;
+        }
+    }
+
+    if(button.recordRTC) {
+        if(button.recordRTC.length) {
+            button.recordRTC[0].stopRecording(function(url) {
+                if(!button.recordRTC[1]) {
+                    button.recordingEndedCallback(url);
+                    stopStream();
+                    saveVideo(button.recordRTC);
+                    // saveToDiskOrOpenNewTab(button.recordRTC[0]);
+                    return;
+                }
+
+                button.recordRTC[1].stopRecording(function(url) {
+                    button.recordingEndedCallback(url);
+                    stopStream();
+                });
+            });
+        }
+        else {
+            button.recordRTC.stopRecording(function(url) {
+                button.recordingEndedCallback(url);
+                stopStream();
+
+                // saveToDiskOrOpenNewTab(button.recordRTC);
+                saveVideo(button.recordRTC);
+            });
+        }
+    }
+});
+
+function captureAudioPlusVideo(config) {
+    captureUserMedia({video: true, audio: true}, function(audioVideoStream) {
+        recordingPlayer.srcObject = audioVideoStream;
+
+        config.onMediaCaptured(audioVideoStream);
+
+        audioVideoStream.onended = function() {
+            config.onMediaStopped();
+        };
+    }, function(error) {
+        config.onMediaCapturingFailed(error);
+    });
+}
+
+function captureUserMedia(mediaConstraints, successCallback, errorCallback) {
+    navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
+}
+
+
+
+function saveVideo(recordRTC){
+    saveRecording.addEventListener('click',function(e){
+        this.disabled = true;
+        startRecording.disabled = true;
+        stopRecording.disabled = true;
+        jQuery('.progress').removeClass('d-none');
+        uploadToServer(recordRTC, function(progress, fileURL) {
+            if(progress === 'ended') {
+                button.disabled = false;
+                button.innerHTML = 'Click to download from server';
+                button.onclick = function() {
+                    window.open(fileURL);
+                };
+                return;
+            }
+            button.innerHTML = progress;
+        });
+    });
+}
+
+function uploadToServer(recordRTC,callback){
+    var blob = recordRTC instanceof Blob ? recordRTC : recordRTC.blob;
+    var fileType = blob.type.split('/')[0] || 'video';
+    var fileName = (Math.random() * 1000).toString().replace('.', '');
+fileName += '.webm';
+    // create FormData
+    var formData = new FormData();
+    formData.append(fileType + '-filename', fileName);
+    formData.append(fileType + '-blob', blob);
+
+    callback('Uploading ' + fileType + ' recording to server.');
+    var upload_url = 'save.php';
+    makeXMLHttpRequest(upload_url, formData, function(progress) {
+        if (progress !== 'upload-ended') {
+            callback(progress);
+            return;
+        }
+
+        // callback('ended', upload_directory + fileName); 
+    });
+}
+
+function makeXMLHttpRequest(url, data, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            callback('upload-ended');
+        }
+    };
+
+    request.upload.onloadstart = function() {
+        callback('Upload started...');
+    };
+
+    request.upload.onprogress = function(event) {
+        jQuery('.progress-bar').css("width",Math.round(event.loaded / event.total * 100) + "%");
+        console.log(Math.round(event.loaded / event.total * 100) + "%");
+        jQuery('.upload-status').text(Math.round(event.loaded / event.total * 100) + "%");
+        jQuery('.progress-bar').attr("aria-valuenow",Math.round(event.loaded / event.total * 100));
+    };
+
+    request.upload.onload = function() {
+        callback('progress-about-to-end');
+    };
+
+    request.upload.onload = function() {
+        callback('progress-ended');
+    };
+
+    request.upload.onerror = function(error) {
+        callback('Failed to upload to server');
+        console.error('XMLHttpRequest failed', error);
+    };
+
+    request.upload.onabort = function(error) {
+        callback('Upload aborted.');
+        console.error('XMLHttpRequest aborted', error);
+    };
+
+    request.open('POST', url);
+    request.send(data);
+}
