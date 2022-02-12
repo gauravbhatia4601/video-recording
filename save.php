3d9aca820fdc8a7b7d25726724eaaacb32ab0c9c
gauravbhatia4601 <gauravbhatia4601@users.noreply.github.com>
2022-02-12 16:25:32 +0530

    Update save.php

--- save.php
+++ save.php
@@ -1,5 +1,6 @@
 <?php
 
+$data = [];
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -12,7 +13,6 @@ function someFunction($errno, $errstr) {
 
 function saveVideo()
 {    
- 
     $file_idx = 'video-blob';
     $fileName = $_POST['video-filename'];
     $tempName = $_FILES[$file_idx]['tmp_name'];
@@ -28,25 +28,28 @@ function saveVideo()
         return;
     }
 
+    if(!is_dir('./uploads')){
+        @mkdir('./uploads');
+    }
+
     $filePath = 'uploads/' . $fileName;
     
     // make sure that one can upload only allowed audio/video files
     $allowed = array(
         'webm',
-        'wav',
         'mp4',
         'mkv',
-        'mp3',
         'ogg'
     );
     $extension = pathinfo($filePath, PATHINFO_EXTENSION);
     if (!$extension || empty($extension) || !in_array($extension, $allowed)) {
+        $data['status'] = 'NOK';
+        $data['error'] = 'Invalid file extension: '.$extension;
         echo 'Invalid file extension: '.$extension;
         return;
     }
     
     if (!move_uploaded_file($tempName, $filePath)) {
-        print_r('here');die;
         if(!empty($_FILES["file"]["error"])) {
             $listOfErrors = array(
                 '1' => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
@@ -60,18 +63,28 @@ function saveVideo()
             $error = $_FILES["file"]["error"];
 
             if(!empty($listOfErrors[$error])) {
+                $data['status'] = 'NOK';
+                $data['error'] = $listOfErrors[$error];
                 echo $listOfErrors[$error];
             }
             else {
+                $data['status'] = 'NOK';
+                $data['error'] = 'Not uploaded because of error #'.$_FILES["file"]["error"];
                 echo 'Not uploaded because of error #'.$_FILES["file"]["error"];
             }
         }
         else {
+            $data['status'] = 'NOK';
+            $data['error'] = 'Problem saving file: '.$tempName;
             echo 'Problem saving file: '.$tempName;
         }
         return;
+    }else{
+        $data['status'] = 'OK';
+        $data['message'] = 'Video Uploaded Successfully!';
     }
-    echo "Video Uploaded";
+    echo json_encode($data);
+    return;
 }
 
 saveVideo();
